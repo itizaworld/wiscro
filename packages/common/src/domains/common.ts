ce6c28e541bb156df916b587abcb77778e526cf0
itizawa <itizawa.pen@gmail.com>
2022-09-18 19:01:27 +0900

    👷 setting lint

--- packages/common/src/domains/common.ts
+++ packages/common/src/domains/common.ts
@@ -1,6 +1,6 @@
 export class User {
-  name:string
-  constructor(name:string){
-    this.name = name
+  name: string;
+  constructor(name: string) {
+    this.name = name;
   }
 }
--- packages/front/pages/index.tsx
+++ packages/front/pages/index.tsx
@@ -1,13 +1,12 @@
-import { hoge } from '@wiscro/common'
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
+import { hoge } from '@wiscro/common';
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
   console.log(hoge);
-  
+
   return (
     <div className={styles.container}>
       <Head>
@@ -22,8 +21,7 @@ const Home: NextPage = () => {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
+          Get started by editing <code className={styles.code}>pages/index.tsx</code>
         </p>
 
         <div className={styles.grid}>
@@ -37,10 +35,7 @@ const Home: NextPage = () => {
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
+          <a href="https://github.com/vercel/next.js/tree/canary/examples" className={styles.card}>
             <h2>Examples &rarr;</h2>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
@@ -50,9 +45,7 @@ const Home: NextPage = () => {
             className={styles.card}
           >
             <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
           </a>
         </div>
       </main>
@@ -70,7 +63,7 @@ const Home: NextPage = () => {
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;

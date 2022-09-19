16bc3a7d21485417b2845f86317f9b3de2724638
itizawa <itizawa.pen@gmail.com>
2022-09-19 14:10:46 +0900

    ✨ implement test hooks

--- packages/front/src/pages/index.tsx
+++ packages/front/src/pages/index.tsx
@@ -2,10 +2,13 @@ import { hoge, User } from '@wiscro/common';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import { useTest } from '../hooks/useTest';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
   console.log(hoge, new User('hoge'));
+  const { data } = useTest();
+  console.log(data);
 
   return (
     <div className={styles.container}>

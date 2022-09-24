54a6d5700d07d7a67e77ec6b48d7d67375bf603e
itizawa <itizawa.pen@gmail.com>
2022-09-24 21:44:27 +0900

    ✨ implement modules

---
+++ packages/back/src/Modules/index.ts
@@ -0,0 +1,1 @@
+export { OgpModule } from './OgpModule';
--- packages/back/src/app.module.ts
+++ packages/back/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OgpModule } from './Modules';
 
 @Module({
-  imports: [],
+  imports: [OgpModule],
   controllers: [AppController],
   providers: [AppService],
 })

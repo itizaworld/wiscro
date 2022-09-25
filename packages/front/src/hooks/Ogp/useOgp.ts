c2864e68ce9bd2cd62dbe8ebb194f6678f196531
itizawa <itizawa.pen@gmail.com>
2022-09-25 17:57:17 +0900

    ✨ instance

--- packages/front/src/hooks/Ogp/useOgp.ts
+++ packages/front/src/hooks/Ogp/useOgp.ts
@@ -8,8 +8,9 @@ import { restClient } from '../../libs/restClient';
  */
 export const useOgp = (url?: string): SWRResponse<Ogp, Error> => {
   const key = url ? `/ogps?url=${url}` : null;
-  return useSWR(key, (endpoint: string) => restClient.apiGet<{ ogp: Ogp }>(endpoint).then((result) => new Ogp(result.data.ogp)), {
-    revalidateOnFocus: false,
-    revalidateOnReconnect: true,
-  });
+  return useSWR(key, (endpoint: string) =>
+    restClient.apiGet<{ ogp: Ogp }>(endpoint).then((result) => {
+      return new Ogp(result.data.ogp);
+    }),
+  );
 };

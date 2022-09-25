020ba5b5921d5b0237f785be2d97248172fa1158
itizawa <itizawa.pen@gmail.com>
2022-09-25 17:57:05 +0900

    👷 set global config

--- packages/front/src/pages/_app.tsx
+++ packages/front/src/pages/_app.tsx
@@ -1,22 +1,26 @@
 import { NextUIProvider } from '@nextui-org/react';
 import type { AppProps } from 'next/app';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import { SWRConfig } from 'swr';
 import { lightTheme, darkTheme } from '../styles';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <NextThemesProvider
-      defaultTheme="dark"
-      attribute="class"
-      value={{
-        light: lightTheme.className,
-        dark: darkTheme.className,
-      }}
-    >
-      <NextUIProvider>
-        <Component {...pageProps} />
-      </NextUIProvider>
-    </NextThemesProvider>
+    // TODO: display toaster
+    <SWRConfig value={{ onError: () => void 0 }}>
+      <NextThemesProvider
+        defaultTheme="dark"
+        attribute="class"
+        value={{
+          light: lightTheme.className,
+          dark: darkTheme.className,
+        }}
+      >
+        <NextUIProvider>
+          <Component {...pageProps} />
+        </NextUIProvider>
+      </NextThemesProvider>
+    </SWRConfig>
   );
 }
 

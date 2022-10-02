f0b34c046ce0caf44aa05d92beb89307e2b65cb0
itizawa <itizawa.pen@gmail.com>
2022-10-02 17:48:42 +0900

    ✨ connect db

--- packages/back/src/app.module.ts
+++ packages/back/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OgpModule } from './Modules';
 
 @Module({
-  imports: [OgpModule],
+  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.MONGO_URL || 'mongodb://localhost:27017/wiscro'), OgpModule],
   controllers: [AppController],
   providers: [AppService],
 })
